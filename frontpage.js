757e3b69d8d127fb1f69180f6c4842bbe2a137ed
Sebastian-lab <59662423+Sebastian-lab@users.noreply.github.com>
2024-02-19 20:40:22 -0800

    trying to fix fade-in

--- frontpage.js
+++ frontpage.js
@@ -2,3 +2,13 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entries.isIntersection){
+            document.querySelectorAll(".animated")[0].classList.add("fade-in");
+        }
+    })
+})
+
+observer.observe(document.querySelector(".contains"));
